dadcb10c98e5668b1db05f240593d3ed2374dc00
sid16 <sidthekid619@gmail.com>
2019-06-24 02:27:03 -0400

    finishing implementation :zap:

--- InterestingAlgorithms/src/insertSort.java
+++ InterestingAlgorithms/src/insertSort.java
@@ -7,5 +7,19 @@
  *
  */
 public class insertSort {
-
+	public void InsertSort(char[] data) {
+		int n = data.length; //best practises fro encapsulation.
+		//Insert
+		for (int i = 1; i < n; i++) { //walk up the array/ insert elements
+			char curr = data[i];
+			
+			//Sort section
+			int j = i;
+			while (j>0 && data[j-1] > curr) {
+				data[j] = data[j-1];
+				j--;
+			}
+			data[j] = curr; //wherever j falls too
+		}
+	}
 }
