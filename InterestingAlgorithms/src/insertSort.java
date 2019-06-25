6d183d84ee3f0fa968e6d8304218f68bafb1b28d
sid16 <sidthekid619@gmail.com>
2019-06-24 21:35:59 -0400

    re-implementation

--- InterestingAlgorithms/src/insertSort.java
+++ InterestingAlgorithms/src/insertSort.java
@@ -3,23 +3,27 @@
  */
 
 /**
+ * This is my O(n^2) implementation for insert-sort.
  * @author sid16
  *
  */
 public class insertSort {
-	public void InsertSort(char[] data) {
-		int n = data.length; //best practises fro encapsulation.
-		//Insert
-		for (int i = 1; i < n; i++) { //walk up the array/ insert elements
-			char curr = data[i];
+	public void InsertSort (char[] data) {
+		//Our strategy: Walk up the array (using for-loop), and sort in non-decreasing order(using while-loop).
+		int n = data.length; //array length
+		//Insert i.e. walk up the array
+		for (int i = 1; i<n; i++) {
+			char currentElement = data[i]; //current element
+			int j = i; //index of current element
+			//Sort
+				//short circuiting here
+			while (j>0 && data[j-1]>currentElement) {
+					data[j] = data[j-1];
+					j--;
+			}//execution of this loop holds until we reach index 0 (for sorting) or prev element <= currentElement
 			
-			//Sort section
-			int j = i;
-			while (j>0 && data[j-1] > curr) {
-				data[j] = data[j-1];
-				j--;
-			}
-			data[j] = curr; //wherever j falls too
+			//When we reach index 0 after sorting, or a case where current > prevElement
+			data[j] = currentElement;
 		}
 	}
 }
