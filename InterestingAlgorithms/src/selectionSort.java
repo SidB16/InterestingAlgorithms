af705dc12b0798bd924df89e01968220f8aad447
sid16 <sidthekid619@gmail.com>
2019-07-06 03:27:13 -0400

    finishing implementation :zap:

--- InterestingAlgorithms/src/selectionSort.java
+++ InterestingAlgorithms/src/selectionSort.java
@@ -12,5 +12,18 @@ public class selectionSort {
 	
 	public void SelectionSort (String[] data) {
 		//Strategy: Walk-up, find min, swap, move-up
+		int n = data.length;
+		for(int i = 0; i < n-1; i++) { //Walk-up
+			int minElement = i; // index of minelement
+			for(int j = i; i < n; j++) { //find min
+				if(data[minElement].compareToIgnoreCase(data[j]) > 0) {
+					minElement = j;
+				}
+			}
+			//Swap min
+			String buffer = data[i];
+			data[i] = data[minElement];
+			data[minElement] = buffer;
+		}
 	}
 }
